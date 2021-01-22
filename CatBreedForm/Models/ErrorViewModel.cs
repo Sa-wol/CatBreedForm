282eaacc16917744becca5ef722add6fbcbe4393
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-01-22 08:17:27 -0800

    Add project files.

---
+++ CatBreedForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CatBreedForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
